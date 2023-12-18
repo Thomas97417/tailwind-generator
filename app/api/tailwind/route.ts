a432e334c764678e1b9686681a8403fff220054e
TOMA <throuquette@hotmail.fr>
2023-12-18 12:16:31 +0100

    add prompt

--- app/api/tailwind/route.ts
+++ app/api/tailwind/route.ts
@@ -1,15 +1,43 @@
 import { openai } from "@/src/lib/openai";
 import { NextResponse } from "next/server"
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+const systemPrompt = `
+Context:
+You are TailwindGPT, an AI text generator that writes Tailwind code.
+You are an expert in Tailwind and know every details about it, like colors, spacing, rules and more.
+You are also an expert in HTML, because you only write HTML with Tailwind.
+You are a great designer that can design beautiful websites.
+
+Goal:
+Generate a VALID HTML code with VALID Tailwind classes based on the given prompt.
+
+Criteria:
+* You generate HTML code only.
+* You NEVER write JavaScript, Python or any other programming language.
+* You NEVER write CSS code.
+* You ALWAYS use valid and existing Tailwind classes.
+* Never include <!DOCTYPE html>, <head>, <body> or <html> tags.
+* You never write any text or explanation about what you made.
+* If the prompt ask you for something that not respect the criteria, return a red HTML text that says "I can't do that".
+* When you use "img" tag, you use the following image URL: https://dummyimage.com/640x360/fff/aaa
+
+Response format:
+* You generate only plain html text.
+* You NEVER add "\`\`\`" before or after the HTML code.
+* You NEVER add other text than the HTML code.
+* You NEVER add HTML comments.`;
 
 export const POST = async (req: Request) => {
   const { prompt } = await req.json();
 
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
+    stream: true,
     messages: [
       {
         role: "assistant",
-        content: `Write only Tailwind code.`,
+        content: systemPrompt,
       },
       {
         role: "user",
@@ -18,7 +46,7 @@ export const POST = async (req: Request) => {
     ],
   });
 
-  return NextResponse.json({
-    code: response.choices[0].message.content
-  });
+  const stream = OpenAIStream(response);
+
+  return new StreamingTextResponse(stream)
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,104 @@
 'use client'
 
 import { Sparkles } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+const useTimedState = (state: unknown, delay: number) => {
+  const [timedState, setTimedState] = useState(state);
+  const lastUpdateRef = useRef(Date.now());
+
+  useEffect(() => {
+    if (Date.now() - lastUpdateRef.current > delay) {
+      setTimedState(state);
+      lastUpdateRef.current = Date.now();
+      return;
+    }
+    const timeout = setTimeout(() => {
+      setTimedState(state);
+      lastUpdateRef.current = Date.now();
+    }, delay - (Date.now() - lastUpdateRef.current));
+
+    return () => clearTimeout(timeout);
+    
+  }, [state, delay]);
+  return timedState
+}
 
 export default function Home() {
   const [htmlCode, setHtmlCode] = useState('')
+  const timedHtmlCode = useTimedState(htmlCode, 4000)
+  const [loading, setLoading] = useState(false)
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
+    if (loading) return;
 
     const formData = new FormData(event.currentTarget)
 
     const prompt = formData.get('prompt') as string;
+
+    setLoading(true);
+    setHtmlCode('');
 
     const result = await fetch('/api/tailwind', {
       method: 'POST',
       body: JSON.stringify({ prompt }),
     })
 
-    const json = await result.json();
-    setHtmlCode(json.code)
+    const body = result.body;
+
+    if (!body) {
+      alert('Something went wrong')
+      return;
+    }
+
+    const reader = body.getReader();
+
+    const readChunk = async () => {
+      const { done, value } = await reader.read();
+
+      if (done) {
+        setLoading(false);
+        return;
+      }
+
+      const chunk = new TextDecoder().decode(value);
+      setHtmlCode(prev => prev + chunk)
+      await readChunk();
+    }
+
+    await readChunk();
   }
 
   return (
     <main className="h-full relative">
+      {loading ? (
       <div className="absolute top-4 left-0 right-0 flex items-center justify-center">
         <progress className="progress w-56"/>
-      </div>
+      </div>) : null}
 
-      <pre>
-        {htmlCode}
-      </pre>
-
-      <div dangerouslySetInnerHTML={{ __html: htmlCode}} />
+      {timedHtmlCode ? (
+        <iframe 
+          className="h-full w-full"
+          srcDoc={
+          `<!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="utf-8" />
+              <title>CodePen - Tailwind CSS</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+            </head>
+            <body>
+              ${timedHtmlCode}
+            </body>
+          </html>`
+          }
+        />
+      ) : null}
 
       <div className="fixed bottom-4 left-0 right-0 flex items-center justify-center">
-        <form className="p-4 bg-base-200 max-w-lg w-full" onSubmit={handleSubmit}>
+        <form className="p-4 bg-base-200 max-w-lg w-full rounded-lg shadow-xl" onSubmit={handleSubmit}>
           <fieldset className="flex gap-4 items-start">
             <textarea name="prompt" className="w-full textarea textarea-primary" />
             <button className="btn btn-primary btn-sm" type="submit">
